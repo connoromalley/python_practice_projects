a1e50e996d862b536367ebd85f24253b38429ec6
ConnorOMalley <william.omalley@umconnect.umt.edu>
2023-11-25 20:59:21 -0700

    updated pig game

--- pig.py
+++ pig.py
@@ -18,4 +18,29 @@ while True:
     else:
         print("Invalid, try again.")
 
-print(players) 
+
+
+max_score = 50
+player_scores = [0 for _ in range(players)]
+
+while max(player_scores) < max_score:
+
+    for player_idx in range(players):
+        current_score = 0
+        while True:
+            should_roll = input("Would you like to roll (y)?")
+            if should_roll.lower() != "y":
+                break
+
+            value = roll()
+            if value == 1:
+                print("You rolled a 1! Turn done!")
+                break
+            else:
+                current_score += value
+                print("You rolled a:", value)
+
+            print("Your score is:", current_score)
+
+
+
